19ce2e37f25fd9ed288e51d36863308e68ec5c4b
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2026-02-12 12:47:17 +0100

    Added some variable declaration

--- pun.c
+++ pun.c
@@ -2,5 +2,10 @@
 
 int main(void) {
   printf("To C, or not to C: that is the question. \n");
+  int height = 10;
+  float decimal = 3.1234443;
+  printf("Your height is %d\n and\n your decimal is %.3f\n", height, decimal);
+
+  
   return 0;
 }
