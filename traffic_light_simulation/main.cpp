ee4ea765d5843e981969280884c702ec4fcf2620
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2026-04-17 16:48:17 +0100

    Updated

--- traffic_light_simulation/main.cpp
+++ traffic_light_simulation/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <windows.h>
+#include <thread>
+#include <chrono>
 using namespace std;
 
-int main () {
-  cout << "Hello world!";
+
+enum TrafficLightState {
+  RED,
+  YELLOW,
+  GREEN
+};
+
+void timer(int seconds){
+
+  Sleep(1000);
+  while(seconds >= 1 ){
+cout<< "\r" <<seconds << flush;
+Sleep(1000);
+seconds--;
+  }
+  cout << endl;
+} 
+
+int main() {
+  TrafficLightState currentState = RED;
+
+  while(true){
+    switch(currentState) {
+      case RED:
+      cout << "\n🔴 Red Light\n";  
+       timer(10);  
+      currentState = YELLOW;
+      break;
+      case YELLOW:
+      cout << "\n🟡 Yellow Light\n";
+       timer(5);
+       currentState = GREEN;
+       break;
+      case GREEN:
+      cout << "\n🟢 Green Light\n";
+       timer(10);
+       currentState = RED;
+       break;
+
+    }
+  }
+
   return 0;
 }
